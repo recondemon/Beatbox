6685701b80dfe3a39c24f1799905492142d9f63a
aileks <aeverly14@pm.me>
2024-09-19 14:52:35 -0400

    WIP adding song data to authenticated landing page

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
   const user = useSelector(state => state.session.user);
-  const data = useLoaderData();
+  const songs = useLoaderData();
 
   if (!user) {
     return (
@@ -14,15 +14,18 @@ const Home = () => {
   }
 
   return (
-    <div
-      className='flex flex-col w-sm my-20 mx-60'
-    >
-      {data.Songs ? (
-        data.Songs.forEach(song => (
-            <p className='text-xl'>{song.title}</p>
+    <div className='flex flex-col w-sm my-20 mx-60'>
+      {songs.length ? (
+        songs.map(song => (
+          <div>
+            <p className='text-xl'>{song.name}</p>
+            <audio controls>
+              <source src={song.url} type="audio/m4a"/>
+            </audio>
+          </div>
         ))
       ) : (
-          <p className='text-xl'>No Songs Yet</p>
+        <p className='text-xl'>No Songs Yet</p>
       )}
     </div>
   );

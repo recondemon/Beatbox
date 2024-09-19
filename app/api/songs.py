8a8d5a32f4b1dea712ad02673a412226dc138c64
aileks <aeverly14@pm.me>
2024-09-19 13:10:37 -0400

    songs route fix

--- app/api/songs.py
+++ app/api/songs.py
@@ -2,11 +2,11 @@ from flask import Blueprint, request
 from app.models import Models, db
 from flask_login import current_user, login_user, logout_user, login_required
 
-Songs = Models.Songs
-auth_routes = Blueprint("songs", __name__)
+# Songs = Models.Songs
+songs = Blueprint("songs", __name__)
 
 
+@songs.route("/")
 @login_required
-@auth_routes.route("/")
 def all_songs():
     return {"Songs": []}

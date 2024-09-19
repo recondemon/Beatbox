d2eba5ce0f2d2476613bb4bbfd5b7de96e6bbe97
aileks <aeverly14@pm.me>
2024-09-19 19:31:54 -0400

    fixed sizing & added comment

--- react-vite/src/components/Home/Sidebar/Sidebar.jsx
+++ react-vite/src/components/Home/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 const Sidebar = () => {
   return (
     <div className='flex flex-col h-full bg-popover rounded-md'>
-      <nav className='flex flex-col p-4'>
-        <p className='border-accent border rounded-sm p-1 w-16 h-16'>A</p>
-        <p className='border-accent border rounded-sm p-1 w-16 h-16'>A</p>
-        <p className='border-accent border rounded-sm p-1 w-16 h-16'>A</p>
+      <nav className='flex flex-col p-4 gap-4'>
+        {/* TODO: Make these links to liked playlists, albums & artists */}
+        <p className='border-accent border rounded-sm p-1 w-14 h-14'>A</p>
+        <p className='border-accent border rounded-sm p-1 w-14 h-14'>A</p>
+        <p className='border-accent border rounded-sm p-1 w-14 h-14'>A</p>
       </nav>
     </div>
   );

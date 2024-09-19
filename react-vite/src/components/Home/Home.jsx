80bf8b4a8cc97f5b0e543aeff1fd485852fdd617
aileks <aeverly14@pm.me>
2024-09-19 18:04:00 -0400

    song cards instead

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 const Home = () => {
   const user = useSelector(state => state.session.user);
   const songs = useLoaderData();
+  console.log(songs);
 
   if (!user) {
     return (
@@ -14,24 +15,34 @@ const Home = () => {
   }
 
   return (
-    <div className='flex flex-col w-sm my-20 mx-60 max-h-[calc(80vh-160px)] overflow-y-auto'>
+    <div className='my-20 mx-40'>
       {songs.length ? (
-        songs.map(song => (
-          <div key={song.id} className='mt-6'>
-            <p className='text-xl'>{song.name}</p>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6'>
+          {songs.map(song => (
+            <div
+              key={song.id}
+              className='bg-card p-6 max-w-72 rounded-lg shadow flex flex-col items-center text-card-foreground border transition-transform transform hover:scale-105 hover:shadow-md hover:cursor-pointer'
+            >
+              <p className='text-xl font-semibold'>{song.name}</p>
 
-            <p className='text-lg'>
-              {songs[0].artist[0].first_name} {songs[0].artist[0].last_name}
-            </p>
+              <p className='text-lg italic'>{song.album[0].name}</p>
 
-            <audio controls>
-              <source
-                src={song.url}
-                type='audio/m4a'
-              />
-            </audio>
-          </div>
-        ))
+              <p className='text-sm'>
+                by {song.artist[0].first_name} {song.artist[0].last_name}
+              </p>
+
+              {/* <audio */}
+              {/*   className='w-full mt-4' */}
+              {/*   controls */}
+              {/* > */}
+              {/*   <source */}
+              {/*     src={song.url} */}
+              {/*     type='audio/m4a' */}
+              {/*   /> */}
+              {/* </audio> */}
+            </div>
+          ))}
+        </div>
       ) : (
         <p className='text-xl'>No Songs Yet</p>
       )}

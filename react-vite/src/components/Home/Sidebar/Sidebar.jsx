0a50f258ffd1c27a5e342e5624871e68e7ec905d
aileks <aeverly14@pm.me>
2024-09-19 19:04:03 -0400

    border stuff

--- react-vite/src/components/Home/Sidebar/Sidebar.jsx
+++ react-vite/src/components/Home/Sidebar/Sidebar.jsx
@@ -2,9 +2,9 @@
 const Sidebar = () => {
   return (
     <div className="flex flex-col gap-1">
-      <p className="border-muted border-[1px] rounded-sm p-1">Sidebar</p>
-      <p className="border-muted border-[1px] rounded-sm p-1">Goes</p>
-      <p className="border-muted border-[1px] rounded-sm p-1">Here</p>
+      <p className="border-muted border rounded-sm p-1">Sidebar</p>
+      <p className="border-muted border rounded-sm p-1">Goes</p>
+      <p className="border-muted border rounded-sm p-1">Here</p>
     </div>
   )
 }

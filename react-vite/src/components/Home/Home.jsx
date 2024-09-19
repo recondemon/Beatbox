dd5370e3046d86cf8cbae90815627b76a0ffd494
aileks <aeverly14@pm.me>
2024-09-19 18:54:51 -0400

    tenative sidebar & tailwind fixes

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useLoaderData } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const Home = () => {
   const user = useSelector(state => state.session.user);
@@ -14,38 +15,44 @@ const Home = () => {
   }
 
   return (
-    <div className='my-20 mx-40'>
-      {songs.length ? (
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6'>
-          {songs.map(song => (
-            <div
-              key={song.id}
-              className='bg-card p-6 max-w-72 rounded-lg shadow flex flex-col items-center text-card-foreground border transition-transform transform hover:scale-105 hover:shadow-md hover:cursor-pointer'
-            >
-              <p className='text-xl font-semibold'>{song.name}</p>
+    <>
+      <div className='mt-14 ml-4 absolute left-0'>
+        <Sidebar />
+      </div>
 
-              <p className='text-lg italic'>{song.album[0].name}</p>
+      <div className='my-20 mx-40'>
+        {songs.length ? (
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6'>
+            {songs.map(song => (
+              <div
+                key={song.id}
+                className='bg-card p-6 max-w-72 rounded-lg shadow flex flex-col items-center text-card-foreground border-muted border-2 transition-transform transform hover:scale-105 hover:shadow-md hover:cursor-pointer'
+              >
+                <p className='text-xl font-semibold'>{song.name}</p>
 
-              <p className='text-sm'>
-                by {song.artist[0].first_name} {song.artist[0].last_name}
-              </p>
+                <p className='text-lg italic'>{song.album[0].name}</p>
 
-              {/* <audio */}
-              {/*   className='w-full mt-4' */}
-              {/*   controls */}
-              {/* > */}
-              {/*   <source */}
-              {/*     src={song.url} */}
-              {/*     type='audio/m4a' */}
-              {/*   /> */}
-              {/* </audio> */}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p className='text-xl'>No Songs Yet</p>
-      )}
-    </div>
+                <p className='text-sm'>
+                  by {song.artist[0].first_name} {song.artist[0].last_name}
+                </p>
+
+                {/* <audio */}
+                {/*   className='w-full mt-4' */}
+                {/*   controls */}
+                {/* > */}
+                {/*   <source */}
+                {/*     src={song.url} */}
+                {/*     type='audio/m4a' */}
+                {/*   /> */}
+                {/* </audio> */}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className='text-xl'>No Songs Yet</p>
+        )}
+      </div>
+    </>
   );
 };
 
---
+++ react-vite/src/components/Home/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col gap-1">
+      <p className="border-muted border-[1px] rounded-sm p-1">Sidebar</p>
+      <p className="border-muted border-[1px] rounded-sm p-1">Goes</p>
+      <p className="border-muted border-[1px] rounded-sm p-1">Here</p>
+    </div>
+  )
+}
+
+export default Sidebar

480f9955462b00647c7d08eb4e46f80403ea92da
aileks <aeverly14@pm.me>
2024-09-20 13:11:51 -0400

    working on song data in album view

--- react-vite/src/components/Home/AlbumDetails/AlbumDetails.jsx
+++ react-vite/src/components/Home/AlbumDetails/AlbumDetails.jsx
@@ -1,16 +1,35 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from 'react-router-dom';
 
 const AlbumDetails = () => {
-  const album = useLoaderData()
-  console.log(album)
+  const album = useLoaderData();
+  const { songs } = album;
+  console.log(songs);
 
   return (
-    <>
+    <div className=' mt-14 bg-secondary rounded-lg border border-border w-full h-full'>
       {album && (
-        <p>Hello World</p>
-      )}
-    </>
-  )
-}
+        <>
+          <h2 className='text-2xl my-4 font-bold'>{album.name}</h2>
+          {songs.map(song => (
+            <div className='flex gap-1 mb-4'>
+              <p className='text-lg'>{song.name}</p>
 
-export default AlbumDetails
+              <audio
+                className='w-full h-12 bg-secondary text-foreground rounded-md shadow-md'
+                controls
+              >
+                <source
+                  className='bg-none text-foreground rounded-md shadow-md'
+                  src={`${song.url}`}
+                  type='audio/m4a'
+                />
+              </audio>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AlbumDetails;

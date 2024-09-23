42cdb4180f0a4b6c26a76c1a063286d86664e40b
Alberto <albertoadent@gmail.com>
2024-09-23 14:20:27 -0400

    updated stores and added genres store

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -3,13 +3,14 @@ import {
   applyMiddleware,
   compose,
   combineReducers,
-} from 'redux';
-import thunk from 'redux-thunk';
-import sessionReducer from './session';
-import songsReducer from './songs';
-import albumsReducer from './albums';
-import playlistsReducer from './playlists';
-import artistsReducer from './artists';
+} from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
+import songsReducer from "./songs";
+import albumsReducer from "./albums";
+import playlistsReducer from "./playlists";
+import artistsReducer from "./artists";
+import genresReducer from "./genres";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
@@ -17,18 +18,20 @@ const rootReducer = combineReducers({
   albums: albumsReducer,
   playlists: playlistsReducer,
   artists: artistsReducer,
+  genres: genresReducer,
 });
 
 let enhancer;
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.MODE === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import('redux-logger')).default;
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = preloadedState => {
+const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 

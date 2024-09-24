73997e3b9497e592bf3297f260552ecc2c84b651
Alberto <albertoadent@gmail.com>
2024-09-24 13:05:03 -0400

    edits in audio player

--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { ModalProvider, Modal } from '../context/Modal';
-import { thunkAuthenticate } from '../redux/session';
-import Navigation from '../components/Navigation/Navigation';
-import Sidebar from '../components/Home/Sidebar';
-import { fetchQueue, selectQueue } from '../redux/playlists';
-import AudioPlayer from '../components/AudioPlayer';
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { ModalProvider, Modal } from "../context/Modal";
+import { thunkAuthenticate } from "../redux/session";
+import Navigation from "../components/Navigation/Navigation";
+import Sidebar from "../components/Home/Sidebar";
+import AudioPlayer from "../components/AudioPlayer";
 
 export default function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const queue = useSelector(selectQueue);
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
 
   useEffect(() => {
-    dispatch(fetchQueue());
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
@@ -25,11 +21,7 @@ export default function Layout() {
         <Navigation />
         <Sidebar />
         {isLoaded && <Outlet />}
-        <AudioPlayer
-          list={{ songs: queue || [] }}
-          currentSongIndex={currentSongIndex}
-          setCurrentSongIndex={setCurrentSongIndex}
-        />
+        <AudioPlayer/>
         <Modal />
       </ModalProvider>
     </>
